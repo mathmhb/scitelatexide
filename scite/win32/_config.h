e6ef1a7e59f60058b15e0ce562894618ccfcb6ce
mathmhb <mathmhb@f9f7334e-3806-11de-8fef-1f5ae417a2df>
2009-05-23 14:45:43 +0000

    [mhb] r40: minor changes
    -- add clean_files.bat: clean *.o and scite*.exe
    -- add upx.exe
    -- add make_with_MinGW_mhb.bat: generate scite*.exe under name "All-In-One IDE"
    -- revise make_with_MingGW.bat: add my onw MINGW+MSYS path; still generate scite*.exe under name "SciTE LaTeX IDE"
    -- add scite/win32/_config.h: editor name is defined here! macro APPNAME will be used in other source files
    -- revise some source files: change string "SciTE LaTeX IDE" to APPNAME; no need to modify them in the future if editor name is changed

---
+++ scite/win32/_config.h
@@ -0,0 +1,13 @@
+/*
+[mhb]: 05/22/09
+Currently this editor is called "SciTE LaTeX IDE" -- a name given by Instanton, focusing on LaTeX IDE.
+However, in the future, it will be eventually named as "All-In-One IDE" with more powerful enhancements. 
+We hope this editor will eventually compete with any text editor.
+Please report any feature requests or bugs so as we can improve this powerful editor further.
+*/
+//#define ONEIDE
+#ifdef ONEIDE 
+#define APPNAME "All-In-One IDE"
+#else
+#define APPNAME "SciTE LaTeX IDE"
+#endif
