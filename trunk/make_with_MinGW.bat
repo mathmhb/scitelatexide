ee76adfe9a40ca3af75f5fb21236fec0e22a854a
mathmhb <mathmhb@f9f7334e-3806-11de-8fef-1f5ae417a2df>
2011-12-28 01:47:20 +0000

    fix tex.abbr

--- trunk/make_with_MinGW.bat
+++ trunk/make_with_MinGW.bat
@@ -18,7 +18,8 @@ make ONEIDE=-DONEIDE ZERO_EMBED=-DZERO_EMBED
 
 cd ..\..
 del Sc1IDE\Sc1.exe
-upx -o Sc1IDE\Sc1.exe scite\bin\SciTE.exe
+REM **upx -o Sc1IDE\Sc1.exe scite\bin\SciTE.exe
+copy scite\bin\SciTE.exe Sc1IDE\Sc1.exe
 
 cd iconlib
 cmd.exe /c call make.cmd
